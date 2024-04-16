de9cd3dd9104b9437b03394e14c4a102d0ab573c
cris <crisjrduarte@gmail.com>
2024-04-15 21:35:32 -0300

    aula sobre a criação e movimentação das entidades

--- pythonProject/jogo do aviao/code/Game.py
+++ pythonProject/jogo do aviao/code/Game.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+
 import pygame as pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
+from code.Level import Level
 from code.Menu import Menu
-from code.const import WIN_HEIGHT, WIN_WIDTH
+from code.const import WIN_HEIGHT, WIN_WIDTH, MENU_OPTION
 
 
 class Game:
@@ -14,10 +17,15 @@ class Game:
         self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
-
         while True:
             menu = Menu(self.window)
-            menu.run()
-            pass
+            menu_return = menu.run()
+
+            if menu_return in [MENU_OPTION[0],MENU_OPTION[1],MENU_OPTION[2]]:
+                level= Level(self.window, 'level1', menu_return)
+                level_return= level.run()
+            else:
+                pygame.quit()
+                sys.exit()
 
 
--- pythonProject/jogo do aviao/code/Menu.py
+++ pythonProject/jogo do aviao/code/Menu.py
@@ -1,25 +1,56 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+import sys
+
 import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.const import WIN_WIDTH
+from code.const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WITHE, COLOR_BLACK, COLOR_RED
 
 
 class Menu:
     def __init__(self, window):
-        self.window : Surface = window
+        self.window: Surface = window
         self.surf = pygame.image.load('./asset/menuBg.png')
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
         pygame.mixer_music.load('./asset/Menu.mp3')
         pygame.mixer_music.play(-1)
+        menu_opition = 0
+
         while True:
-            self.window.blit(source=self.surf , dest=self.rect)
-            self.menu_text(50,'GAME of PLANE', (255, 128, 0), (WIN_WIDTH / 2, 70))
-            self.menu_text(50,'GAME of PLANE', (255, 128, 0), (WIN_WIDTH / 2, 70))
+            #desenhar na tela
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, 'GAME', COLOR_BLACK, (WIN_WIDTH / 2, 30))
+            self.menu_text(50, 'of', COLOR_BLACK, (WIN_WIDTH / 2, 60))
+            self.menu_text(50, 'PLANE', COLOR_BLACK, (WIN_WIDTH / 2, 90))
+
+            for i in range(len(MENU_OPTION)):
+                if i == menu_opition:
+                    self.menu_text(30, MENU_OPTION[i], COLOR_RED, ((WIN_WIDTH / 2), 200 + 35 * i))
+                else:
+                    self.menu_text(30, MENU_OPTION[i], COLOR_WITHE, ((WIN_WIDTH / 2), 200 + 35 * i))
+            # verificar eventos
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        if menu_opition < len(MENU_OPTION) -1:
+                            menu_opition += 1
+                        else:
+                            menu_opition = 0
+
+                    if event.key == pygame.K_UP:
+                         if menu_opition > 0:
+                            menu_opition -= 1
+                         else:
+                            menu_opition = len(MENU_OPTION) - 1
+                    if event.key == pygame.K_RETURN:
+                         return MENU_OPTION[menu_opition]
 
             pygame.display.flip()
 
